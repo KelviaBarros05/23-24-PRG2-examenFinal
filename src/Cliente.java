c664456ab812f8b39e2ba8cb55fea737d158d8cf
KelviaBarros05 <barroskelvia122@gmail.com>
2024-06-13 12:31:57 +0200

    Avances clase Cliente. Nueva clase Menu

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 class Cliente{
     private String nombre;
     private int numeroPedido;
 
-    public Cliente (String nombre, int numero pedido){
-        
+    public Cliente (String nombre, int numeroPedido){
+        this.nombre = nombre;
+        this.numeroPedido= numeroPedido;
+    }
+
+    public void hacerPedido{
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, dime como quieres tu hamburguesa: ");
+        System.out.println(menu);
+        String entrada = scanner.nextLine();
+
+    }
+
+    public static void main(String[] args) {
+
+        Cliente cliente = Cliente("Manuel", 207);
+
+        cliente.hacerPedido();
+        pedido.mostrar();
     }
 }
